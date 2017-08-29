72e5a4e93d6cc9a67efcbca6c6535927379141fa
thinkWind <512869343@qq.com>
2017-08-29 09:02:11 +0800

    Initial commit

---
+++ Example/TKDataStorage/TKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TKAppDelegate.h
+//  TKDataStorage
+//
+//  Created by 512869343@qq.com on 08/29/2017.
+//  Copyright (c) 2017 512869343@qq.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface TKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/TKDataStorage/TKViewController.h
@@ -0,0 +1,13 @@
+//
+//  TKViewController.h
+//  TKDataStorage
+//
+//  Created by 512869343@qq.com on 08/29/2017.
+//  Copyright (c) 2017 512869343@qq.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface TKViewController : UIViewController
+
+@end
